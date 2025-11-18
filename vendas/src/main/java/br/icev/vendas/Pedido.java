e33acfc18cfe94a85c03fbe672472627e1d1b669
Gustavo19-code <gusta23316@gmail.com>
2025-11-18 13:14:34 -0300

    correção da classe pedido

--- vendas/src/main/java/br/icev/vendas/Pedido.java
+++ vendas/src/main/java/br/icev/vendas/Pedido.java
@@ -1,18 +1,32 @@
 package br.icev.vendas;
 
+import br.icev.vendas.excecoes.QuantidadeInvalidaException;
+
 import java.math.BigDecimal;
 import java.util.Map;
 
 public class Pedido {
-    public enum Status { PAGO }
+    private final BigDecimal totalPago;
+    private final String codigoAutorizacao;
+   private final Status status;
+   private final Map<String,Integer> itensPorCodigo;
 
-    public Pedido(Map<String, Integer> itensPorCodigo, BigDecimal totalPago,
-                  String codigoAutorizacao, Status status) {
-        throw new UnsupportedOperationException("TODO");
+   public enum Status{
+       PAGO,
+
+   }
+
+    public Pedido(Map<String, Integer> itensPorCodigo, BigDecimal totalPago, String codigoAutorizacao, Status status) {
+        this.itensPorCodigo=itensPorCodigo;
+        this.totalPago=totalPago;
+        this.codigoAutorizacao=codigoAutorizacao;
+        this.status=status;
     }
 
-    public BigDecimal getTotalPago() { throw new UnsupportedOperationException("TODO"); }
-    public String getCodigoAutorizacao() { throw new UnsupportedOperationException("TODO"); }
-    public Status getStatus() { throw new UnsupportedOperationException("TODO"); }
-    public int getQuantidadeItem(String codigo) { throw new UnsupportedOperationException("TODO"); }
+    public BigDecimal getTotalPago() { return totalPago; }
+    public String getCodigoAutorizacao() { return codigoAutorizacao; }
+    public Status getStatus() { return status; }
+    public int getQuantidadeItem(String codigo) {return itensPorCodigo.getOrDefault(codigo,0);}
+
+
 }
